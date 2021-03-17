b8bfaa4e516b34a688a042280a135e5643715528
road-pig <qiuzeeming@gmail.com>
2021-03-17 19:25:45 +0800

    profile screen now shows actual info

--- app/src/main/java/com/example/covidgreenpass/ui/profile/EditDetailsFragment.java
+++ app/src/main/java/com/example/covidgreenpass/ui/profile/EditDetailsFragment.java
@@ -1,4 +1,4 @@
-package com.example.covidgreenpass;
+package com.example.covidgreenpass.ui.profile;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.covidgreenpass.R;
 
 /**
  * A simple {@link Fragment} subclass.
--- app/src/main/java/com/example/covidgreenpass/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/covidgreenpass/ui/profile/ProfileFragment.java
@@ -16,18 +16,34 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.covidgreenpass.R;
+import com.example.covidgreenpass.model.User.Users;
 
 public class ProfileFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
-    private Button updateDetailsButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        updateDetailsButton = root.findViewById(R.id.profileUpdate);
+
+        Button updateDetailsButton = root.findViewById(R.id.profileUpdate);
+        TextView profileName = root.findViewById(R.id.profileName);
+        TextView profileID = root.findViewById(R.id.profileID);
+        TextView profileVaccineName = root.findViewById(R.id.profileVaccineName);
+        TextView profileVaccinationDate = root.findViewById(R.id.profileVaccinationDate);
+        TextView profileTestResult = root.findViewById(R.id.profileTestResult);
+        TextView profileTestDate = root.findViewById(R.id.profileTestDate);
+        TextView profileContactNumber = root.findViewById(R.id.profileContactNumber);
+        TextView profileAddress = root.findViewById(R.id.addressTextView);
+
+        profileName.setText(Users.loggedInUser.getName());
+        profileVaccineName.setText(Users.loggedInUser.getVaccine());
+        profileID.setText(Users.loggedInUser.getID());
+        profileVaccinationDate.setText(Users.loggedInUser.getVaccineDate());
+        profileTestResult.setText(Users.loggedInUser.getTestResult());
+        profileTestDate.setText(Users.loggedInUser.getTestDate());
+        profileAddress.setText(Users.loggedInUser.getAddress());
+        profileContactNumber.setText(Users.loggedInUser.getContactNo());
+
         updateDetailsButton.setOnClickListener(view -> {
             NavHostFragment.findNavController(this).navigate(R.id.action_navigation_profile_to_editDetailsFragment);
         });
