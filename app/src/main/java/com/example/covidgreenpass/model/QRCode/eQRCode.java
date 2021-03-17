7c11bc0676bcfc5d644758f7ecbc1de7b309322a
Olive <oliverysfx@outlook.com>
2021-03-17 18:32:50 +0800

    change qrcode class to taking a user object instead of different strings

--- app/src/main/java/com/example/covidgreenpass/model/QRCode/eQRCode.java
+++ app/src/main/java/com/example/covidgreenpass/model/QRCode/eQRCode.java
@@ -1,22 +1,16 @@
 package com.example.covidgreenpass.model.QRCode;
 
 import com.example.covidgreenpass.MainActivity;
+import com.example.covidgreenpass.model.User.User;
 
 import java.nio.file.Paths;
 
 public class eQRCode extends QRCode {
-    private String username, password, name, address, contactNo, contactEmail;
-    private Boolean vaccinated;
+    private User user;
 
-    public eQRCode(int width, int height, String username, String password, String name, String address, String contactNo, String contactEmail, Boolean vaccinated) {
+    public eQRCode(int width, int height, User user) {
         super(width, height);
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.address = address;
-        this.contactNo = contactNo;
-        this.contactEmail = contactEmail;
-        this.vaccinated = vaccinated;
+        this.user = user;
     }
 
     public void generate(MainActivity mainActivity){
@@ -30,13 +24,7 @@ public class eQRCode extends QRCode {
     @Override
     public String toString() {
         return "eQRCode{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", contactNo='" + contactNo + '\'' +
-                ", contactEmail='" + contactEmail + '\'' +
-                ", vaccinated=" + vaccinated +
+                "user=" + user +
                 '}';
     }
 }
