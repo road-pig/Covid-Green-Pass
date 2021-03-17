93a40688f30866a82ba0d571e40c08ccda839b6f
road-pig <qiuzeeming@gmail.com>
2021-03-17 16:12:48 +0800

    fixed bottom nav

--- app/src/main/java/com/example/covidgreenpass/MainActivity.java
+++ app/src/main/java/com/example/covidgreenpass/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.covidgreenpass;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -20,11 +21,12 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_qr_code, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
 }
--- app/src/main/java/com/example/covidgreenpass/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/covidgreenpass/ui/profile/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,25 +12,25 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.covidgreenpass.R;
 
-public class NotificationsFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
+    private Button updateDetailsButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-//        final TextView textView = root.findViewById(R.id.text_notifications);
-//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-////            public void onChanged(@Nullable String s) {
-////                textView.setText(s);
-////            }
-//        });
+        updateDetailsButton = root.findViewById(R.id.profileUpdate);
+        updateDetailsButton.setOnClickListener(view -> {
+            NavHostFragment.findNavController(this).navigate(R.id.action_navigation_profile_to_editDetailsFragment);
+        });
         return root;
     }
 }
