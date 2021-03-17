c4a875af4ec8ac0c4290a164ccc1369fb5ef5c0e
road-pig <qiuzeeming@gmail.com>
2021-03-17 19:09:45 +0800

    qr code completed

--- app/src/main/java/com/example/covidgreenpass/ui/QRCode/QRCodeFragment.java
+++ app/src/main/java/com/example/covidgreenpass/ui/QRCode/QRCodeFragment.java
@@ -1,5 +1,7 @@
 package com.example.covidgreenpass.ui.QRCode;
 
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +15,14 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.covidgreenpass.MainActivity;
 import com.example.covidgreenpass.R;
+import com.example.covidgreenpass.model.QRCode.eQRCode;
+import com.example.covidgreenpass.model.User.Users;
+
+
+import java.io.File;
+import java.nio.file.Paths;
 
 public class QRCodeFragment extends Fragment {
 
@@ -30,6 +39,14 @@ public class QRCodeFragment extends Fragment {
 
         ImageView qrCodeImage = view.findViewById(R.id.qrCodeImageView);
 
+        String filename = "./" + Paths.get("./").toAbsolutePath().relativize(Paths.get(getActivity().getFilesDir().getAbsolutePath())) + "/" + "current" + ".png";
+
+        File image = new File(filename);
+        if (!image.exists()) {
+            eQRCode qrCode = new eQRCode(300, 300, Users.loggedInUser);
+            qrCode.generate((MainActivity) getActivity());
+        }
+        qrCodeImage.setImageURI(Uri.fromFile(image));
     }
 
 }
